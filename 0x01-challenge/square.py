aed3efd3a50574a5f233c6bf72d15822b7659cf2
MokhtarTouiri <59121449+MokhtarTouiri@users.noreply.github.com>
2021-11-19 00:51:20 +0100

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+""" square file """
+
 
 class square():
-    
+    """ square class """
     width = 0
     height = 0
 
-    
+
     def __init__(self, *args, **kwargs):
+        """ constructor """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -21,7 +24,7 @@ class square():
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
+    """square main"""
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
